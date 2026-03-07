9f4a5549fe9ae71bbabf737dc9e40e36a5f47fe0
dat04032002 <anhdatdz04@gmail.com>
2026-03-07 22:57:26 +0700

    updaye

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer: React.FC = () => {
         <div className="flex justify-center items-center gap-2 mb-4">
           <Heart size={20} className="text-rose-500 animate-pulse" fill="#f43f5e" />
         </div>
-        <h3 className="text-2xl font-script text-white mb-2">Thành Đạt & Thu Hà</h3>
+        <h3 className="text-2xl font-script text-white mb-2">Lê Thực & Nguyễn Nhung</h3>
         <p className="text-sm">Cảm ơn bạn đã ghé thăm website đám cưới của chúng mình.</p>
         <div className="mt-8 pt-8 border-t border-stone-800 text-xs">
           <p>© 2024 Wedding4u. Designed with love.</p>
